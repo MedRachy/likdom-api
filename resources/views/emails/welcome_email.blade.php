ed1234758fecc2029d5e5d1098e1daa0add2433a
Mahrachy Mohamed <mahrachymohamed@gmail.com>
2023-05-14 12:24:50 +0100

    14/05

--- resources/views/emails/welcome_email.blade.php
+++ resources/views/emails/welcome_email.blade.php
@@ -1,9 +1,35 @@
-@component('mail::message')
-    # Bienvenu {{ $userName }} chez Likdom
+@component('mail::layout')
+    {{-- Header --}}
+    @slot('header')
+        @component('mail::header', ['url' => 'https://likdom.ma'])
+            <img src="https://likdom.ma/storage/Shape@2x.png" alt="likdom logo" class="logo">
+        @endcomponent
+    @endslot
 
-    l'équipe de likdom vous souhaite le bienvenu.
-    Si vous avez des questions ou besoin d'assistance,
-    n'hésitez pas à nous contacter à l'adresse : support@likdom.ma
+    {{-- Body --}}
+    <h1>Bienvenu chez Likdom</h1>
 
-    {{ config('app.name') }}
+    <p>L'équipe de likdom vous souhaite le bienvenu.
+        Nous proposons des passages ponctuels ou des abonnements à travers des offres préétablies, nous mettons à votre
+        disposition des agents qualifiés et formés qui travaillent pour assurer votre satisfaction.
+
+        Si vous avez des questions ou besoin d'assistance,
+        n'hésitez pas à nous contacter à l'adresse support@likdom.ma
+    </p>
+
+    {{-- Subcopy --}}
+    {{-- @isset($subcopy)
+        @slot('subcopy')
+            @component('mail::subcopy')
+                {{ $subcopy }}
+            @endcomponent
+        @endslot
+    @endisset --}}
+
+    {{-- Footer --}}
+    @slot('footer')
+        @component('mail::footer')
+            © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+        @endcomponent
+    @endslot
 @endcomponent
